19e2ada85badf4ef8b45f4963e77173c50fc0a99
Tammer Galal <tammergalal20@gmail.com>
2018-08-27 11:54:57 -0400

    changed App to a class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement('h1', {}, "Sirius"),
-    React.createElement('h2', {}, "Dog"),
-    React.createElement('h2', {}, "Chocolate Lab")
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed)
   ]);
 }
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement('h1', {}, 'Adoption Purfected!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
-  ]);
+class App extends React.Component {
+  render(){
+    return React.createElement("div", {}, [
+      React.createElement('h1', {}, 'Adoption Purfected!'),
+      React.createElement(Pet, {
+        name: "Sirius",
+        animal: "Dog",
+        breed: "Australian Cattle Dog"
+      }),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Petunia",
+        animal: "Cat",
+        breed: "Short Hair"
+      })
+    ]);
+  }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
