6d2ea5383bbf804a44e1c4b4e06739cf0fca2df9
Tammer Galal <tammergalal20@gmail.com>
2018-08-27 14:45:08 -0400

    Adding in some more rules and refactoring into JSX

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
     alert("You clicked the title");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adoption Purfected!"
-      ),
-      React.createElement(Pet, {
-        name: "Sirius",
-        animal: "Dog",
-        breed: "Australian Cattle Dog"
-      }),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Petunia",
-        animal: "Cat",
-        breed: "Short Hair"
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adoption Purfected!</h1>
+        <Pet name="Sirius" animal="Dog" breed="Australian Cattle Dog" />
+        <Pet name="Petunia" animal="Cat" breed="Mixed" />
+        <Pet name="Molly" animal="Dog" breed="Who Knows" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
