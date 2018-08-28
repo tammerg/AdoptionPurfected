d04292e25ca2adc479615dee80ad786dc09ad516
Tammer Galal <tammergalal20@gmail.com>
2018-08-28 18:15:59 -0400

    added emotion styling to my header and header link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import pf from "petfinder-client";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import Navbar from "./Navbar";
 import { Provider } from "./SearchContext";
 
 const petfinder = pf({
@@ -72,14 +73,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <Navbar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
---
+++ src/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "@reach/router";
+import styled from "react-emotion";
+import colors from "./colors";
+
+const Container = styled("header")`
+  background-color: ${colors.dark};
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Navbar = () => (
+  <Container>
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
+      <span aria-label="search" role="img">
+        🔍
+      </span>
+    </NavLink>
+  </Container>
+);
+
+export default Navbar;
